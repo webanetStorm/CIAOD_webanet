317dadfa52d47d6c46499e5e4cb7c8965b9bfbb6
webanet <my2712@mail.ru>
2024-03-13 23:53:44 +0300

    СИАОД 4 начало

---
+++ 4/4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+struct Question
+{
+
+    int themeNumber;
+
+    string questionText;
+
+    int points;
+
+    vector<string> answerOptions;
+
+    int correctAnswerIndex;
+
+};
+
+
+struct QuestionTable
+{
+
+    vector<Question> questions;
+
+};
+
+
+int main()
+{
+    return 0;
+}
