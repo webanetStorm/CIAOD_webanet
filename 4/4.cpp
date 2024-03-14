2f8003045f8c3ddaf5792df491d9f1ecc52df641
webanet <my2712@mail.ru>
2024-03-14 23:29:25 +0300

    4 сиаод, ?

--- 4/4.cpp
+++ 4/4.cpp
@@ -1,23 +1,67 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <format>
 
 
 using namespace std;
 
 
+const int MAX_QUESTION_LENGTH = 100;
+const int MAX_NUM_QUESTIONS = 5;
+
+
+
 struct Question
 {
 
-    int themeNumber;
+    int ThemeId;
 
-    string questionText;
+    char QuestionText[MAX_QUESTION_LENGTH];
 
-    int points;
+    int Points;
 
-    vector<string> answerOptions;
+    char AnswerOptions[MAX_NUM_QUESTIONS];
 
-    int correctAnswerIndex;
+    int CorrectAnswerIndex;
+
+
+
+    Question ( 
+        int themeId, 
+        const char questionText[MAX_QUESTION_LENGTH],
+        int points, 
+        const char answerOptions[MAX_NUM_QUESTIONS], 
+        int correctAnswerIndex 
+    )
+    {
+        ThemeId = themeId;
+        CorrectAnswerIndex = correctAnswerIndex;
+        Points = points % 100;
+
+        strncpy_s( QuestionText, questionText, _TRUNCATE );
+        strncpy_s( AnswerOptions, answerOptions, _TRUNCATE );
+    }
+
+    Question() = default;
+
+
+
+    string ToFormatString()
+    {
+        string options;
+
+        for ( int i = 0; i < MAX_NUM_QUESTIONS; i++ )
+            options += format( "\n\t{}. {}", i, AnswerOptions[i] );
+
+        return format( "|{:^10}|{:<100s}|{:^6}|{}| Правильный ответ: {}",
+            ThemeId,
+            QuestionText,
+            Points,
+            AnswerOptions,
+            CorrectAnswerIndex
+        );
+    }
 
 };
 
@@ -25,12 +69,39 @@ struct Question
 struct QuestionTable
 {
 
-    vector<Question> questions;
+    vector<Question> Questions;
+
+
+    QuestionTable() = default;
+
+    void Echo()
+    {
+        cout << format( "|{:^10}|{:^100s}|{:^6}|\n{:=^118}\n",
+            "Theme",
+            "Question",
+            "Points",
+            "" 
+        );
+
+        for ( int i = 0; i < Questions.size(); i++ )
+            cout << Questions.at( i ).ToFormatString() << endl;
+    }
+
+    void AddQuestion( const Question& question )
+    {
+        Questions.push_back( question );
+    }
 
 };
 
 
+
 int main()
 {
+    setlocale( LC_ALL, "" );
+    Question q( 1, "2 + 2 = ?", 10, { "1", "2", "3", "4" }, 4);
+    cout << q.ToFormatString();
+
+
     return 0;
 }
